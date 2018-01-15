05a61c2d7000812f5999039f2b3cdb3f5fe59031
StevanCakic <stevancakic93@gmail.com>
2018-01-15 20:26:50 +0100

    initial login form and user model created

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+
+const md5 = require("md5");
+const validator = require("validator");
+const mongodbErrorHandler = require("mongoose-mongodb-errors");
+const passporLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, "Invalid email address"],
+    required: "Please Supply and email address"
+  },
+  name: {
+    type: String,
+    required: "Please supply a name",
+    trim: true
+  }
+});
+
+userSchema.plugin(passporLocalMongoose, { usernameField: "email" });
+userSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model("User", userSchema);
