dfa84dfc0d3182066f10fe4b021e7776b9502fb3
StevanCakic <stevancakic93@gmail.com>
2018-02-05 21:19:49 +0100

    created top restorants page

--- routes/index.js
+++ routes/index.js
@@ -61,7 +61,13 @@ router.get(
   authController.isLoggedIn,
   catchErrors(storeController.getHearts)
 );
-router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview))
+router.post(
+  "/reviews/:id",
+  authController.isLoggedIn,
+  catchErrors(reviewController.addReview)
+);
+
+router.get("/top", catchErrors(storeController.getTopStores));
 
 /* 
   API
